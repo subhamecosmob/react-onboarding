0d592a9de7e03f97aaadb501f9414349179e2dc4
subham <subhamkashyapp@gmail.com>
2022-02-17 20:16:53 +0530

    conditional rendering

---
+++ src/Conditionalrendering.jsx
@@ -0,0 +1,19 @@
+import data from "./data.json"
+
+
+const renderList = () => {
+    console.log(data)
+    return data.map((todo )=>{
+       if(todo.completed) return <h1 key={todo.id} style={{color:"green"}}>{todo.title}</h1>;
+        return <h1 key={todo.id} style={{color:"red"}}>{todo.title}</h1>;
+    });
+
+    
+}  
+
+export const Conditionalrendering = () => {
+
+  return (
+    <div>{renderList()}</div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import  ReactDOM  from "react-dom";
 
 import React, { useState } from 'react'
 import List from "./List";
+import { Conditionalrendering } from "./Conditionalrendering";
 
 export const Reactcomponent = () => {
 
@@ -12,6 +13,6 @@ export const Reactcomponent = () => {
     <input value={input} onChange={(e)=>setInput(e.target.value)} />
   )}
 
-ReactDOM.render(<List/> , document.querySelector("#root"))
+ReactDOM.render(<Conditionalrendering/> , document.querySelector("#root"))
 
 

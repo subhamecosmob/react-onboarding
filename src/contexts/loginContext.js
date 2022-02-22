a64252464fef7e505155548010c33858cc83309b
subham <subhamkashyapp@gmail.com>
2022-02-22 17:05:25 +0530

    adding simple animation inside react app using css keyframe

--- src/contexts/loginContext.js
+++ src/contexts/loginContext.js
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 export const LoginContext = createContext();
 
 export const LoginProvider = (props) => {
-	let [isLogin, setIslogin] = useState(true);
+	let [isLogin, setIslogin] = useState(false);
 
 	return (
 		<LoginContext.Provider value={{ isLogin, setIslogin }}>
